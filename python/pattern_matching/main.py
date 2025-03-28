ed22952d4577d3b6f19b294fd34b1d2e9095d3f5
James Anderson <james@tisteltech.com>
2025-03-28 13:17:33 -0400

    code exercises
    python pattern matching
    
    playing around

--- python/pattern_matching/main.py
+++ python/pattern_matching/main.py
@@ -1,11 +1,47 @@
 '''
 Explore pattern matching.
 '''
+import os
+import pickle
+
+import requests
+
+
+def fetch_test_data(url):
+    'be nice to test api pickle result to reduce calls to api.'
+    fname = 'api_data.pickle'
+    result = None
+
+    if os.path.exists(fname):
+        with open(fname, 'rb') as f:
+            result = pickle.load(f)
+    else:
+        result = requests.get(url).json()
+        with open(fname, 'wb') as f:
+            pickle.dump(result, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+    return result
+
+
+def handle_command(msg):
+    'handle_command'
+    match msg:
+        case {'symbol': 'ETHBTC', **rest}:
+            print(rest)
+        case _:
+            pass
 
 
 def run():
     'main function'
-    print('hello, world')
+    print('start')
+    res = fetch_test_data('https://api4.binance.com/api/v3/ticker/24hr')
+    print(len(res))
+    # print(f'res: {res[:3]}')
+
+    for r in res:
+        handle_command(r)
+    print('end')
 
 
 if __name__ == '__main__':
