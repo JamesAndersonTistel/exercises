2da3cd42ccd09f65aca1220763e9e5868c23c642
James Anderson <james@tisteltech.com>
2025-04-05 12:45:24 -0400

    fluent python exercises
    data classes, named tuple

--- python/data_classes/main.py
+++ python/data_classes/main.py
@@ -1,10 +1,33 @@
 '''
 Data Classes
+
+>>> Coordinate = namedtuple('Coordinate', 'lat long')
+>>> issubclass(Coordinate, tuple)
+True
+>>> moscow = Coordinate(55.76, 37.62)
+>>> moscow
+Coordinate(lat=55.76, long=37.62)
+>>> loc = Coordinate(55.76, 37.62)
+>>> moscow == loc
+True
 '''
 
-def run():
-    print('Hello')
+from collections import namedtuple
+
+class Coordinate:
+    """
+    >>> moscow = Coordinate(55.76, 37.62)
+    >>> loc = Coordinate(55.76, 37.62)
+    >>> loc == moscow
+    False
+    >>> # compared object ids!
+    """
+
+    def __init__(self, lat: float, long: float):
+        self._lat = lat
+        self._long = long
 
 
 if __name__ == '__main__':
-    run()
+    import doctest
+    doctest.testmod()
