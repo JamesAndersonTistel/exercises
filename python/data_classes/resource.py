dc318eae15240a3c6cfbe303fcead5eeff552f4d
James Anderson <james@tisteltech.com>
2025-04-06 17:50:45 -0400

    fluent python exercises
    data classes resources

---
+++ python/data_classes/resource.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass, field
+from typing import Optional
+from enum import Enum, auto
+from datetime import date
+
+class ResourceType(Enum):
+    BOOK = auto()
+    EBOOK = auto()
+    VIDEO = auto()
+
+
+@dataclass
+class Resource:
+    '''media description'''
+
+    identifier: str
+    title: str = '<untitled>'
+    creators: list[str] = field(default_factory=list)
+    date: Optional[date] = None
+    type: ResourceType = ResourceType.BOOK
+    description: str = ''
+    language: str = ''
+    subjects: list[str] = field(default_factory=list)
+
+    
+    
