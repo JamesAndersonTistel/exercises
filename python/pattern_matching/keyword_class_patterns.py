56a35f456a40698202a9d0dd87c60cf068ac0fcd
James Anderson <james@tisteltech.com>
2025-04-07 11:32:54 -0400

    fluent python exercises
    match keyword class patterns

--- python/pattern_matching/keyword_class_patterns.py
+++ python/pattern_matching/keyword_class_patterns.py
@@ -1,1 +1,44 @@
+"""
+"""
 
+import typing
+
+
+class City(typing.NamedTuple):
+    continent: str
+    name: str
+    country: str
+
+
+cities = [
+    City('Asia', 'Tokyo', 'JP'),
+    City('Asia', 'Delhi', 'IN'),
+    City('North America', 'Mexico City', 'MX'),
+    City('North America', 'New York', 'US'),
+    City('South America', 'São Paulo', 'BR'),
+]
+
+
+def match_asian_cities():
+    '''
+    >>> match_asian_cities()
+    [City(continent='Asia', name='Tokyo', country='JP'), City(continent='Asia', name='Delhi', country='IN')]
+    '''
+    results = []
+
+    for city in cities:
+        match city:
+            case City(continent='Asia'):
+                results.append(city)
+    
+    return results
+
+
+def run1():
+    print()
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+
+    
