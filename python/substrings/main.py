f99869315c77de29d0d78acb25a1262f1ad9ba37
James Anderson <james@tisteltech.com>
2025-04-12 21:28:21 -0400

    code gen elixir - start with tests
    anagram

--- python/substrings/main.py
+++ python/substrings/main.py
@@ -3,8 +3,8 @@ def run(s):
     for i in range(len(s)):
         for j in range(len(s) - i):
             x = s[j:i+j+1]
-            print(f'x: {x}')
-    
+            print(f'x: {x} {''.join(sorted(x))}')
+
 
 if __name__ == '__main__':
-    run('This is a test')
+    run('zThisa')
